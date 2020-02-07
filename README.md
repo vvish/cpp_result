49f805b82470cfd0216a2864e1443bf9b6555912
vvish <v.v.swe.os@gmail.com>
2020-02-07 10:49:19 +0100

    Initial code commit

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# result
+The built-in integral types are used as a low-level result storage that should provide efficient access and manipulation via bit operations.
+The storage type can be specified to achieve portability and efficiency for various platforms.
+
+## Motivation
+
+The framework makes an attempt to provide resource efficient ways to define error types and facilities to analyze them.
+In a resource constrained environment it would be convenient to have facilities to represent errors and ability to combine several of them
+without overhead of memory allocation/deallocation.
+
+The type is supposed to be project defined and concrete result values can be declared closer to the usage. That will enable clients to use unified methods to analyze and forward
+errors up to the call stack while providing some encapsulation.
+
+
+## Introduction
+
+The framework allows clients to specify result code along with a set of error categories that can be used to describe the possible error in a more precise way. The underlying type can be specified
+as a template parameter.
+
+In the example the type to store the result code is defined using 8-bit integral type.
+The error type is supposed to have the following structure:
+
+2 bits for domain, 2 bits for sub-domain. 4 bits for error code
+
+
+```c++
+namespace application
+{
+//project-wide definition of error types
+
+//definition of some categories and their widths in bits
+//(here 2 bits allowing maximum of 4 categories on each level)
+MAKE_RESULT_CATEGORY(Domain, 2);
+MAKE_RESULT_CATEGORY(SubDomain, 2);
+
+// definition of the result type (contains two categories
+// defined above and based on 8 bit integer
+// the rest 4 bits are used for result code)
+MAKE_RESULT_TYPE(TestResult, uint8_t, Domain, SubDomain);
+
+//definition of the error domains and subdomains
+
+// here the error can occure in the application layer
+// domain having value 3 is defined
+constexpr auto Application = Domain{3};
+
+// two subdomains defined
+constexpr auto Server = SubDomain{1};
+constexpr auto Client = SubDomain{2};
+
+//error codes for concrete errors
+constexpr uint8_t rpcErrorCode = 1;
+constexpr uint8_t backendAccessErrorCode = 2;
+
+//local alias for the positive result
+constexpr auto Ok = TestResult::success;
+
+//error definitions consisting of categories (Domain, SubDomain) and error codes
+constexpr auto rpcClientError = TestResult::make(Application, Client, rpcErrorCode);
+constexpr auto backendAccessError = TestResult::make(Application, Client, backendAccessErrorCode);
+
+} // namespace application
+
+```
+
---
+++ test/result_test.cpp
@@ -0,0 +1,304 @@
+#include "result.hpp"
+
+#include <gtest/gtest.h>
+
+namespace result_tests
+{
+TEST(Resuls, ManualDeclaration_8_BitWidth)
+{
+    struct TestDomain {};
+    struct TestSubDomain {};
+
+    using Domain = result::category<TestDomain, 2>;
+    using SubDomain = result::category<TestSubDomain, 3>;
+
+    using TestResult = result::result_t<uint8_t, Domain, SubDomain>;
+
+    TestResult r{0b01111101};
+
+    EXPECT_EQ(result::get_category<Domain>(r).value, 0b01);
+    EXPECT_EQ(result::get_category<SubDomain>(r).value, 0b111);
+    EXPECT_EQ(result::get_code(r), 0b101);
+}
+
+MAKE_RESULT_CATEGORY(Domain, 2);
+MAKE_RESULT_CATEGORY(SubDomain, 2);
+MAKE_RESULT_TYPE(TestResult, uint8_t, Domain, SubDomain);
+
+namespace test_errors
+{
+namespace drivers
+{
+constexpr auto Drivers = Domain{0};
+constexpr auto Ethernet = SubDomain{1};
+
+constexpr uint8_t linkErrorCode = 3;
+
+constexpr auto Ok = TestResult::success;
+constexpr auto ethLinkError
+    = TestResult::make(Drivers, Ethernet, linkErrorCode);
+}  // namespace drivers
+
+namespace networking
+{
+constexpr auto Networking = Domain{1};
+constexpr auto TcpIpStack = SubDomain{1};
+
+constexpr uint8_t connectionAbortedErrorCode = 3;
+
+constexpr auto Ok = TestResult::success;
+constexpr auto connectionAbortedError
+    = TestResult::make(Networking, TcpIpStack, connectionAbortedErrorCode);
+}  // namespace networking
+
+namespace infrastructure
+{
+constexpr auto Infrastructure = Domain{2};
+constexpr auto Remote = SubDomain{1};
+
+constexpr uint8_t messageSendingErrorCode = 1;
+constexpr uint8_t messageReceivingErrorCode = 2;
+
+constexpr auto Ok = TestResult::success;
+
+constexpr auto messageSendingError
+    = TestResult::make(Infrastructure, Remote, messageSendingErrorCode);
+
+}  // namespace infrastructure
+
+namespace application
+{
+constexpr auto Application = Domain{3};
+constexpr auto Server = SubDomain{1};
+constexpr auto Client = SubDomain{2};
+
+constexpr uint8_t rpcErrorCode = 1;
+constexpr uint8_t backendAccessErrorCode = 2;
+
+constexpr auto Ok = TestResult::success;
+
+constexpr auto rpcClientError
+    = TestResult::make(Application, Client, rpcErrorCode);
+constexpr auto backendAccessError
+    = TestResult::make(Application, Client, backendAccessErrorCode);
+
+}  // namespace application
+}  // namespace test_errors
+
+using aggregate_result = result::aggregate_result_t<uint32_t, TestResult>;
+
+TEST(AggregateError_4x8bit, Intialized_with_one_error)
+{
+    aggregate_result e(test_errors::drivers::ethLinkError);
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[0]), test_errors::drivers::Drivers);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[0]), test_errors::drivers::Ethernet);
+    EXPECT_EQ(result::get_code(e[0]), test_errors::drivers::linkErrorCode);
+
+    EXPECT_EQ(result::get_category<Domain>(e[1]), 0);
+    EXPECT_EQ(result::get_category<SubDomain>(e[1]), 0);
+    EXPECT_EQ(result::get_code(e[1]), 0);
+
+    EXPECT_EQ(result::get_category<Domain>(e[2]), 0);
+    EXPECT_EQ(result::get_category<SubDomain>(e[2]), 0);
+    EXPECT_EQ(result::get_code(e[2]), 0);
+
+    EXPECT_EQ(result::get_category<Domain>(e[3]), 0);
+    EXPECT_EQ(result::get_category<SubDomain>(e[3]), 0);
+    EXPECT_EQ(result::get_code(e[3]), 0);
+}
+
+TEST(AggregateError_4x8bit, Intialized_with_one_error_appended_with_second)
+{
+    aggregate_result e(test_errors::drivers::ethLinkError);
+    e.append(test_errors::networking::connectionAbortedError);
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[0]), test_errors::drivers::Drivers);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[0]), test_errors::drivers::Ethernet);
+    EXPECT_EQ(result::get_code(e[0]), test_errors::drivers::linkErrorCode);
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[1]),
+        test_errors::networking::Networking);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[1]),
+        test_errors::networking::TcpIpStack);
+    EXPECT_EQ(
+        result::get_code(e[1]),
+        test_errors::networking::connectionAbortedErrorCode);
+
+    EXPECT_EQ(result::get_category<Domain>(e[2]), 0);
+    EXPECT_EQ(result::get_category<SubDomain>(e[2]), 0);
+    EXPECT_EQ(result::get_code(e[2]), 0);
+
+    EXPECT_EQ(result::get_category<Domain>(e[3]), 0);
+    EXPECT_EQ(result::get_category<SubDomain>(e[3]), 0);
+    EXPECT_EQ(result::get_code(e[3]), 0);
+}
+
+TEST(
+    AggregateError_4x8bit,
+    Default_Initialized_and_four_errors_appended_via_shift_operator)
+{
+    namespace te = test_errors;
+    aggregate_result e;
+
+    e << te::drivers::ethLinkError << te::networking::connectionAbortedError
+      << te::infrastructure::messageSendingError
+      << te::application::rpcClientError;
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[0]), test_errors::drivers::Drivers);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[0]), test_errors::drivers::Ethernet);
+    EXPECT_EQ(result::get_code(e[0]), test_errors::drivers::linkErrorCode);
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[1]),
+        test_errors::networking::Networking);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[1]),
+        test_errors::networking::TcpIpStack);
+    EXPECT_EQ(
+        result::get_code(e[1]),
+        test_errors::networking::connectionAbortedErrorCode);
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[2]),
+        test_errors::infrastructure::Infrastructure);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[2]),
+        test_errors::infrastructure::Remote);
+    EXPECT_EQ(
+        result::get_code(e[2]),
+        test_errors::infrastructure::messageSendingErrorCode);
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[3]),
+        test_errors::application::Application);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[3]),
+        test_errors::application::Client);
+    EXPECT_EQ(result::get_code(e[3]), test_errors::application::rpcErrorCode);
+}
+
+TEST(
+    AggregateError_4x8bit,
+    Initialized_with_two_errors_values_can_be_accessed_via_iterator)
+{
+    aggregate_result e{
+        test_errors::drivers::ethLinkError,
+        test_errors::networking::connectionAbortedError};
+    aggregate_result::error_iterator_t it(e), end;
+
+    ASSERT_NE(it, end);
+
+    EXPECT_EQ(result::get_category<Domain>(*it), test_errors::drivers::Drivers);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(*it), test_errors::drivers::Ethernet);
+    EXPECT_EQ(result::get_code(*it), test_errors::drivers::linkErrorCode);
+
+    ++it;
+    ASSERT_NE(it, end);
+
+    EXPECT_EQ(
+        result::get_category<Domain>(*it), test_errors::networking::Networking);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(*it),
+        test_errors::networking::TcpIpStack);
+    EXPECT_EQ(
+        result::get_code(*it),
+        test_errors::networking::connectionAbortedErrorCode);
+
+    ++it;
+    ASSERT_EQ(it, end);
+}
+
+TEST(AggregateError_4x8bit, Initialized_with_four_errors_loop_via_iterator)
+{
+    constexpr aggregate_result e{
+        test_errors::drivers::ethLinkError,
+        test_errors::networking::connectionAbortedError,
+        test_errors::infrastructure::messageSendingError,
+        test_errors::application::rpcClientError};
+
+    constexpr TestResult results[]
+        = {test_errors::drivers::ethLinkError,
+           test_errors::networking::connectionAbortedError,
+           test_errors::infrastructure::messageSendingError,
+           test_errors::application::rpcClientError};
+
+    auto i = 0;
+    for (const auto it : e.iterate_errors()) {
+        EXPECT_EQ(
+            result::get_category<Domain>(it),
+            result::get_category<Domain>(results[i]));
+        EXPECT_EQ(
+            result::get_category<SubDomain>(it),
+            result::get_category<SubDomain>(results[i]));
+        EXPECT_EQ(result::get_code(it), result::get_code(results[i++]));
+    }
+
+    EXPECT_EQ(i, 4);
+}
+
+using aggregate_result_replace_topmost = result::aggregate_result_t<
+    uint32_t,
+    TestResult,
+    result::detail::replace_topmost<uint32_t, TestResult>>;
+
+TEST(
+    AggregateError_4x8bit_Replace_Topmost,
+    Default_initialized_and_four_errors_appended_via_shift_operator)
+{
+    namespace te = test_errors;
+    aggregate_result_replace_topmost e;
+
+    e << te::drivers::ethLinkError << te::networking::connectionAbortedError
+      << te::infrastructure::messageSendingError
+      << te::application::rpcClientError;
+
+    e << te::application::backendAccessError;
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[0]), test_errors::drivers::Drivers);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[0]), test_errors::drivers::Ethernet);
+    EXPECT_EQ(result::get_code(e[0]), test_errors::drivers::linkErrorCode);
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[1]),
+        test_errors::networking::Networking);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[1]),
+        test_errors::networking::TcpIpStack);
+    EXPECT_EQ(
+        result::get_code(e[1]),
+        test_errors::networking::connectionAbortedErrorCode);
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[2]),
+        test_errors::infrastructure::Infrastructure);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[2]),
+        test_errors::infrastructure::Remote);
+    EXPECT_EQ(
+        result::get_code(e[2]),
+        test_errors::infrastructure::messageSendingErrorCode);
+
+    EXPECT_EQ(
+        result::get_category<Domain>(e[3]),
+        test_errors::application::Application);
+    EXPECT_EQ(
+        result::get_category<SubDomain>(e[3]),
+        test_errors::application::Client);
+    EXPECT_EQ(
+        result::get_code(e[3]),
+        test_errors::application::backendAccessErrorCode);
+}
+
+}  // namespace result_tests
